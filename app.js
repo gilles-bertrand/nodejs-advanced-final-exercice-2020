cec98909b7e316c8f578b952075bc8eb3c336915
gilles-bertrand <gillestriptyk.eu>
2020-09-07 11:36:42 +0200

    add files

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const hbs = require('express-hbs');
+const router = require('./app.router');
+const app = express();
+//View engine setupr for express HBS
+app.engine('hbs',hbs.express4({
+    partialsDir :[`${process.cwd()}/views/partials`],
+    defaultLayout:`${process.cwd()}/views/layouts/main.hbs`
+}))
+app.set('view engine','hbs');
+app.set('views',`${process.cwd()}/views`);
+
+app.use((req,res,next)=>{
+    console.log(req.method, req.url)
+    next();
+})
+
+app.use(router);
+module.exports = app;
---
+++ start.js
@@ -0,0 +1,30 @@
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
+if (major < 10 || major === 10 && minor <= 2) {
+    console.log('The node version of the server is too low for modern node programming')
+    throw ('The node version of the server is too low for modern node programming')
+}
+//inititalize env variables
+require('dotenv').config({ path: '.variables.env' });
+
+// Async initialization of my server
+
+const initServer = async () => {
+    //Launch Mongo Connection
+    const mongoose = require('mongoose');
+    mongoose.Promise = global.Promise;
+    try {
+        await mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
+        console.log('Mongo is now connected to your app')
+
+    } catch (err) {
+        if (err) throw err;
+    }
+    //Start our app if everything is allright and running
+    const app = require(`${process.cwd()}/app`);
+    app.set('port', process.env.PORT || 8001);
+    const server = app.listen(app.get('port'), () => {
+        console.log(`express running - PORT ${server.address().port}`)
+    })
+}
+initServer();
+
